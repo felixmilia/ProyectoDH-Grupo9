1bfc85940fe0c3ba7412ae7e9933466df67a4369
felixmilia <fmiliaotano@udesa.edu.ar>
2024-06-13 16:16:17 -0300

    agregue el js de detalle

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 fetch("https://fakestoreapi.com/products")
+
+.then(function(res){
+    return res.json()
+})
+
+.then(function (data){
+console.log(data);
+let section = document.querySelector("section")
+let articulosRopa = ''
+
+for(let i = 0; i < data.results.length; i++) {
+
+articulosRopa = articulosRopa + `<article class="ropa_hombre">
+    <a href="./detalles.html?id=${data.results[i].id}">
+     <img src=${data.results[i].image}>
+     </a>
+        <p>${data.results[i].name}</p>
+        <p>${data.results[i].name}</p>
+        </article>`
+}
+section.innerHTML = articulosRopa
+
+})
+
+.catch(function (err){
+console.log(err);
+})
